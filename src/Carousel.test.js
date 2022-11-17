d0e2330b471a8fff40cb28222c004505cbff7aa1
anela <anela89@gmail.com>
2022-11-17 09:59:47 -0800

    Wrote smoke and snapshot test for Carousel

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,27 @@ import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
 
+// smoke test, does it render?
+it('renders without crashing', function() {
+  render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+});
+
+// snapshot test, checks for regressions
+it('matches snapshot', function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  expect(container).toMatchSnapshot();
+})
+
 it("works when you click on the right arrow", function() {
   const { container } = render(
     <Carousel
