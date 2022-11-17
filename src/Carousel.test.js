64fdb18636bd35afa437675c9b13dc5bef045b95
Jason <jasoncmcvicar@gmail.com>
2022-11-17 12:23:19 -0600

    wrote test to show that left arrow is failing

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -23,6 +23,7 @@ it('matches snapshot', function() {
   expect(container).toMatchSnapshot();
 })
 
+// tests right arrow
 it("works when you click on the right arrow", function() {
   const { container } = render(
     <Carousel
@@ -48,5 +49,46 @@ it("works when you click on the right arrow", function() {
   ).not.toBeInTheDocument();
   expect(
     container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
+
+// tests left arrow
+it("works when you click on the left arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+
+  // move backwards in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-cirlce");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
   ).toBeInTheDocument();
 });
